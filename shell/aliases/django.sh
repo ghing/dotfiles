d8f804dc5692e4795e7daf6bd5650ea245698034
Geoff Hing <geoffhing@gmail.com>
2013-10-17 13:08:59 +0000

    Aliases for Django's manage.py script

---
+++ shell/aliases/django.sh
@@ -0,0 +1,7 @@
+# Aliases for Django's manage.py 
+alias mpy="python manage.py"
+alias mpyt="python manage.py test"
+alias mpycs="python manage.py collectstatic"
+alias mpycmp="python manage.py compress"
+alias mpys="python manage.py shell"
+
