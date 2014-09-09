4caabb4a84adb45e13ce5751798305bad467857d
Geoff Hing <geoffhing@gmail.com>
2014-09-09 16:33:40 -0500

    Update datestr alias to use ISO 9601 format

--- shell/aliases/datestr.sh
+++ shell/aliases/datestr.sh
@@ -1,3 +1,3 @@
 # Output a date string suitable for datestamping filenames
 # Usage: tar zcf ~/backup/archive-`datestr`.tar.gz /dir/to/archive
-alias datestr='date +"%Y%m%d-%H%M%S"'
+alias datestr='date +"%Y%m%dT%H%M%S"'
