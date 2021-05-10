61eda1a125535283ea6cd16ce96fe0cce9112250
Geoffrey Hing <geoffhing@gmail.com>
2021-05-10 10:35:48 -0500

    Use powerline installed using pipx

--- shell/powerline.sh
+++ shell/powerline.sh
@@ -1,17 +1,8 @@
 # Powerline
 
-if [[ $platform == 'macosx' ]]; then
-  POWERLINE_ROOT="$HOME/Library/Python/2.7/lib/python/site-packages/powerline"
-else
-  POWERLINE_ROOT="$HOME/.local/lib/python3.8/site-packages/powerline"
-fi
+POWERLINE_ROOT=$(pipx runpip powerline-status show powerline-status | grep 'Location:' | sed  's/Location: //g')/powerline
 
 if [ -d "$POWERLINE_ROOT" ]; then 
-  if [[ $platform == 'macosx' ]]; then
-    export PATH=$PATH:~/Library/Python/2.7/bin/
-  else
-    export PATH=$PATH:~/.local/bin
-  fi
   powerline-daemon -q
   POWERLINE_BASH_CONTINUATION=1
   POWERLINE_BASH_SELECT=1
